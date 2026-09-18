63e47da5e692703f311b017dcdabe3df58a9baee
Joseph Tuffin <35460454+JosephTuffin@users.noreply.github.com>
2026-09-18 14:23:38 +0100

    removed old generator utils

--- src/generator/utils.ts
+++
@@ -1,9 +0,0 @@
-export const camelCase = (str: string, underscoreLeadingDigits = false) =>
-    str
-        .toLowerCase()
-        .replace(underscoreLeadingDigits ? /([-_][a-z])/g : /([-_][a-z0-9])/g, (group) =>
-            group.toUpperCase().replace("-", "").replace("_", ""),
-        );
-
-export const pascalCase = (str: string, underscoreLeadingDigits = false) =>
-    camelCase(str, underscoreLeadingDigits).replace(/^[a-z0-9]/, (char) => char.toUpperCase());
